305ce3fed5fd35d93f51d2da26e3e884b4598da0
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-04-19 17:47:34 +1000

    basic site setup

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google"
+import "@/styles/tailwind.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Julian Sommer",
+  description: "Julian Sommer Portfolio",
+
+  icons: {
+    icon: "favicon.ico",
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
--- src/pages/index.jsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <p>I Only Just Bought The Domain No Judge</p>
-  )
-}
