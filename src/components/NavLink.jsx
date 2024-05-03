6917a55c96ddf7e323045a9da22d8bbedfc33f5e
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2024-05-03 15:00:30 +1000

    start navbar

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function NavLink({ href, title }) {
+  return (
+    <Link
+      href={href}
+      className="block rounded py-2 pl-3 pr-4 text-[#ADB7BE] hover:text-white sm:text-xl md:p-0">
+      {title}
+    </Link>
+  )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
 import Link from "next/link"
+import NavLink from "./NavLink"
+
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+]
 
 export default function Navbar() {
   return (
     <nav>
       <div className="mx-auto flex flex-wrap items-center justify-between p-8">
-        <Link href="/" className="text-5xl font-semibold text-white">
+        <Link href="/" className="text-lg font-semibold text-white md:text-5xl">
           LOGO
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul>
-            <li>
-              <Link
-                href="#about"
-                className="block rounded py-2 pl-3 pr-4 text-[#ADB7BE] hover:text-white sm:text-xl md:p-0">
-                About
-              </Link>
-            </li>
+          <ul className="mt-0 flex p-4 md:flex-row md:space-x-8 md:p-0">
+            {navLinks.map((link) => (
+              <li key={link.title}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
