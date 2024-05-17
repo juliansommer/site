75842939be7704859943443ad0db50edc05cbffb
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-05-17 14:05:34 +1000

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,25 @@ import { ThemeProvider } from "@/components/ThemeProvider"
 const space_grotesk = Space_Grotesk({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Julian Sommer",
-  description: "Julian Sommer Portfolio",
+  applicationName: "Julian Sommer Site",
+  authors: {
+    name: "Julian Sommer",
+  },
+  description: "Julian Sommer Site",
+  keywords: ["Julian Sommer", "Julian Sommer Site"],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  title: "Julian Sommer Site",
+  openGraph: {
+    title: "Julian Sommer",
+    description: "Julian Sommer Site",
+    url: "https://julians.au",
+    siteName: "Julian Sommer",
+    locale: "en_AU",
+    type: "website",
+  },
 }
 
 export default function RootLayout({
