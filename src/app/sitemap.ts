786b47c3b13c7543502de1efc9369a7d2cbf72e3
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-07-02 12:11:31 +1000

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://julians.au',
+      url: "https://julians.au",
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: "monthly",
       priority: 1,
     },
   ]
