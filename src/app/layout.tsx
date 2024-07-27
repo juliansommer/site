287aace784287488f96afb8e8a0b30f2b9770e6f
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-07-27 10:55:17 +1000

    temp disable website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,39 +12,39 @@ export const viewport: Viewport = {
 
 // order of this doesnt matter as next puts the important stuff (viewport, themecolour, title description) first
 // then puts the others alphabeticised
-export const metadata: Metadata = {
-  metadataBase: new URL("https://julians.au"),
-  title: {
-    template: "%s | Julian Sommer",
-    default: "Julian Sommer",
-  },
-  description: "Julian Sommer Portfolio Website",
-  applicationName: "Julian Sommer Site",
-  keywords: ["Julian", "Sommer", "Julian Sommer"],
-  creator: "Julian Sommer",
+// export const metadata: Metadata = {
+//   metadataBase: new URL("https://julians.au"),
+//   title: {
+//     template: "%s | Julian Sommer",
+//     default: "Julian Sommer",
+//   },
+//   description: "Julian Sommer Portfolio Website",
+//   applicationName: "Julian Sommer Site",
+//   keywords: ["Julian", "Sommer", "Julian Sommer"],
+//   creator: "Julian Sommer",
 
-  // not defining title and description in og as next will use the metadata.title and metadata.description
-  // so can update these on page and it will also update the open graph and twitter cards
-  // twitter is not defined as next does it automatically with the title and description so can keep all 3 consistent easily
-  openGraph: {
-    siteName: "Julian Sommer",
-    locale: "en_AU",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-}
+//   // not defining title and description in og as next will use the metadata.title and metadata.description
+//   // so can update these on page and it will also update the open graph and twitter cards
+//   // twitter is not defined as next does it automatically with the title and description so can keep all 3 consistent easily
+//   openGraph: {
+//     siteName: "Julian Sommer",
+//     locale: "en_AU",
+//     type: "website",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     nocache: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       noimageindex: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+// }
 
 export default function RootLayout({
   children,
@@ -60,7 +60,7 @@ export default function RootLayout({
             defaultTheme="dark"
             disableTransitionOnChange>
             {children}
-            <ThemeButton />
+            {/* <ThemeButton /> */}
           </ThemeProvider>
         </body>
       </head>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Experience from "@/components/Experience"
-import Footer from "@/components/Footer"
-import HeroSection from "@/components/HeroSection"
-import Navbar from "@/components/Navbar"
-import Projects from "@/components/Projects"
-import Skills from "@/components/Skills"
+// import Experience from "@/components/Experience"
+// import Footer from "@/components/Footer"
+// import HeroSection from "@/components/HeroSection"
+// import Navbar from "@/components/Navbar"
+// import Projects from "@/components/Projects"
+// import Skills from "@/components/Skills"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -14,20 +14,24 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="min-h-screen overflow-hidden bg-white dark:bg-black">
-      <div className="relative bg-grid-black/[0.02] dark:bg-grid-white/[0.02]">
-        <div className="mx-auto max-w-7xl p-5">
-          <Navbar />
-          <HeroSection />
-        </div>
-        <div className="absolute -bottom-5 left-0 h-10 bg-gradient-to-t from-white dark:from-black xl:h-32"></div>
-      </div>
-      <div className="mt-15 mx-auto max-w-7xl p-5">
-        <Skills />
-        <Projects />
-        <Experience />
-        <Footer />
-      </div>
+    <div className="items-center">
+      <p className="text-center font-extrabold">Remaking, website back soon</p>
     </div>
+
+    // <div className="min-h-screen overflow-hidden bg-white dark:bg-black">
+    //   <div className="relative bg-grid-black/[0.02] dark:bg-grid-white/[0.02]">
+    //     <div className="mx-auto max-w-7xl p-5">
+    //       <Navbar />
+    //       <HeroSection />
+    //     </div>
+    //     <div className="absolute -bottom-5 left-0 h-10 bg-gradient-to-t from-white dark:from-black xl:h-32"></div>
+    //   </div>
+    //   <div className="mt-15 mx-auto max-w-7xl p-5">
+    //     <Skills />
+    //     <Projects />
+    //     <Experience />
+    //     <Footer />
+    //   </div>
+    // </div>
   )
 }
