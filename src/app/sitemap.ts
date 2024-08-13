7b253e3272fb5a6d7104e21af33be11991e8a65e
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-08-13 16:29:38 +1000

    make sitemap weekly so google will start indexing it again

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://julians.au",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     },
   ]
