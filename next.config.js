ad69d4a6146914acbe7fb914ffb224eddf32938f
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-08-21 09:29:24 +1000

    remove next bundle

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import("next").NextConfig} */
-import withBundleAnalyzer from "@next/bundle-analyzer"
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-})
-
 const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
 }
 
-export default bundleAnalyzer(nextConfig)
+export default nextConfig
