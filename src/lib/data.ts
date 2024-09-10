bd542447302fac02a91bb210eacf935c10d3a371
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-09-10 11:03:43 +1000

    change work data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -23,7 +23,7 @@ export const experience = [
     company: "Metatrail",
     description: [
       "●	Optimized SEO, performance (50 increase on Lighthouse), JavaScript bundle size (500kb -> 300kb) on the frontend",
-      "●	Optimized page loading and requests using React by making the GraphQL queries only run-on tab change",
+      "●	Created a Go program to parse the DOM of websites against technologies matches using JavaScript",
       "●	Created Python script to scrape and store website metadata and icons from a million domains",
       "●	Used Next.js with TypeScript to dynamically create web pages for the most popular categories and vendors",
       "●	Used GitHub Actions with Docker to containerize the website and upload the image to GitHub Packages for CI/CD",
