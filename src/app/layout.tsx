0dc3dc8360e789604fd4c7afd1a8d0d41cc88f45
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-11-04 13:55:45 +1000

    update to eslint flat config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import ThemeButton from "@/components/ThemeButton"
-import { ThemeProvider } from "@/components/ThemeProvider"
 import type { Metadata } from "next"
+import { ThemeProvider } from "next-themes"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 
