c6657b7061cc0ad7dfe93071dbee9a42886bde0c
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-03-25 13:42:38 +1100

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@
 // @ts-nocheck
 import eslint from "@eslint/js"
 import nextPlugin from "@next/eslint-plugin-next"
-import reactCompiler from "eslint-plugin-react-compiler"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config(
@@ -10,15 +9,8 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
     plugins: {
       "@next/next": nextPlugin,
-      "react-compiler": reactCompiler,
     },
     rules: {
       ...nextPlugin.configs["core-web-vitals"].rules,
@@ -44,7 +36,12 @@ export default tseslint.config(
         },
       ],
       "@typescript-eslint/require-await": "off",
-      "react-compiler/react-compiler": "error",
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 )
