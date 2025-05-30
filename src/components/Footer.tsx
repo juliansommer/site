d7907497ba22a81be298b4f4aae3753ba4728802
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-05-30 11:58:17 +1000

    add footer bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar"
 
 export default function Footer() {
   return (
-    <div className="border-t md:mt-10">
+    <div className="border-t text-white md:mt-10">
       <Navbar className="flex-col gap-5" />
     </div>
   )
