658f672ea0e2b750329335ab2ef54a4813ac1048
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-06-03 14:39:28 +1000

    make csp only have unsafe-eval in dev

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next"
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' ${
+    process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+  } 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src 'self' blob: data:;
   font-src 'self';
